f99e4b7a60930f5b6427c7e5ccaab4673d839fd7
Mr-Mercury-P <puliprabhasreddy@gmail.com>
2025-07-19 14:41:51 +0530

    completed video 6

--- src/main/java/com/example/demo/controller/QuizController.java
+++ src/main/java/com/example/demo/controller/QuizController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,12 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+// for testing purpose http://localhost:8080/quiz/create?category=Java&numQ=5&title=JQuiz
+
 @RestController
 @RequestMapping("quiz")
 public class QuizController {
+    @Autowired
+    QuizService quizService;
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title)
     {
-        return new ResponseEntity<>("success", HttpStatus.OK);
+        return quizService.createQuiz(category, numQ, title);
     }
 }
--- src/main/java/com/example/demo/dao/QuestionDao.java
+++ src/main/java/com/example/demo/dao/QuestionDao.java
@@ -3,6 +3,8 @@ package com.example.demo.dao;
 
 import com.example.demo.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,7 @@ import java.util.List;
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
         List<Question> findByCategory(String category);
+
+        @Query(value = "select * from question q where q.category = :category order by rand() limit :numQ", nativeQuery = true)
+        List<Question> findRandomQuestionsByCategory(@Param("category") String category, @Param("numQ") int numQ);
 }
